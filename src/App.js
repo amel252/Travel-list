e00b8e854f1f39a3cfa8ad090e8a6865b99e09cb
Amel252 <amelghanem25@gmail.com>
2026-01-27 00:52:46 +0100

    Création de nos components + affichage page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import "./index.css";
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em> 🧳You have X items on your list , and you already packed x</em>
+        </footer>
+    );
+}
+
+export default Stats;
