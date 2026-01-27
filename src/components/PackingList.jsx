662963c1eff7ef64abd74c00d13546fe6e53079c
Amel252 <amelghanem25@gmail.com>
2026-01-27 01:22:02 +0100

    Création du component Item + ajout des élements (packingList)

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Item from "./Item";
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Chargeur", quantity: 1, packed: true },
+    // { id: 4, description: "Socks", quantity: 12, packed: false },
+];
 function PackingList() {
-    return <div className="list">List</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default PackingList;
